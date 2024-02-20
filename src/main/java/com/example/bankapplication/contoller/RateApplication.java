084bdeb36ef9197ebbdf2dd824fad921baed3658
vera3 <vera3899@gmail.com>
2024-02-21 00:11:00 +0300

    javaDoc

--- src/main/java/com/example/bankapplication/contoller/RateApplication.java
+++ src/main/java/com/example/bankapplication/contoller/RateApplication.java
@@ -16,34 +16,67 @@ public class RateApplication {
 
     private final RateBAService rateBAService;
 
+    /**
+     * Метод предназначен для извлечения сущности RateBA по id
+     *
+     * @param id уникальный идентификатор сущности RateBA, по которому будет производиться поиск
+     * @return  Метод возвращает объект типа ResponseEntity, который содержит сущность RateBA, если она была найдена по заданному ID, или возвращает статус "NOT FOUND" (404), если сущность с указанным ID не была найдена.
+     */
+
     @GetMapping("/getRate")
-    public ResponseEntity<RateBA> getRateBA(@RequestParam("id") Long id){
+    public ResponseEntity<RateBA> getRateBA(@RequestParam("id") Long id) {
         System.out.println(rateBAService.getRateBA(id));
-        return ResponseEntity.ok( rateBAService.getRateBA(id));
+        return ResponseEntity.ok(rateBAService.getRateBA(id));
     }
 
+    /**
+     * Метод предназначен для получения всех сущностей
+     *
+     * @return  Метод возвращает объект типа ResponseEntity, который содержит сущности RateBA
+     */
+
     @GetMapping("/getRateAll")
-    public ResponseEntity<List<RateBA>> getRateBAAll(){
+    public ResponseEntity<List<RateBA>> getRateBAAll() {
         System.out.println(rateBAService.getAllRateBA());
         return ResponseEntity.ok(rateBAService.getAllRateBA());
     }
+
+    /**
+     * Метод служит для вызова меотда создания новой сущности
+     *
+     * @param rateBA которую необходимо создать
+     * @return ResponseEntity со статусом NO CONTENT после успешного создания
+     */
+
     @PostMapping("/createRate")
-    public ResponseEntity<Void>createRateBA(@RequestBody RateBA rateBA){
+    public ResponseEntity<Void> createRateBA(@RequestBody RateBA rateBA) {
         System.out.println("create");
         rateBAService.createRateBA(rateBA);
         return ResponseEntity.noContent().build();
     }
 
+    /**
+     * Метод для вызова метода обновления сущности
+     *
+     * @param id Идентификатор объекта RateBA, который необходимо обновить
+     * @param percent Новое процентное значение, которое необходимо установить
+     * @return ResponseEntity содержащий обновленный объект RateBA в случае успеха, или статус НЕ НАЙДЕНО, если объект с указанным идентификатором не найден
+     */
 
     @PatchMapping("/updateRate/{id}")
-    public ResponseEntity<RateBA> updateRateBA(@PathVariable("id") Long id, @RequestParam Double percent){
-        System.out.println(rateBAService.updateRateBA(id,percent));
-       return ResponseEntity.ok(rateBAService.updateRateBA(id,percent));
+    public ResponseEntity<RateBA> updateRateBA(@PathVariable("id") Long id, @RequestParam Double percent) {
+        System.out.println(rateBAService.updateRateBA(id, percent));
+        return ResponseEntity.ok(rateBAService.updateRateBA(id, percent));
     }
 
-
+    /**
+     * Метод для вызова метода по удалению сущности по id
+     *
+     * @param id Идентификатор объекта RateBA, который необходимо удалить
+     * @return ResponseEntity со статусом NO CONTENT после успешного удаления
+     */
     @DeleteMapping("/deleteRate/{id}")
-    public ResponseEntity<Void> deleteRateBA(@PathVariable("id") Long id){
+    public ResponseEntity<Void> deleteRateBA(@PathVariable("id") Long id) {
         System.out.println("delete");
         rateBAService.deleteRateBA(id);
         return ResponseEntity.noContent().build();
--- src/main/java/com/example/bankapplication/service/RateBAService.java
+++ src/main/java/com/example/bankapplication/service/RateBAService.java
@@ -13,25 +13,51 @@ public class RateBAService {
 
     private final RateRepository rateRepository;
 
+    /**
+     * Получает сущность RateBA по её id
+     *
+     * @param id Идентификатор сущности RateBA.
+     * @return Сущность RateBA, если найдена, или выбрасывает исключение, если сущность не найдена
+     */
     public RateBA getRateBA(Long id){
         return rateRepository.getRateBAById(id);
     }
-
+    /**
+     * Получает список всех сущностей RateBA
+     *
+     * @return Список всех сущностей RateBA
+     */
     public List<RateBA> getAllRateBA(){
         return rateRepository.findAll();
     }
 
-
+    /**
+     * Создает новую сущность RateBA
+     *
+     * @param rateBA Новая сущность RateBA для создания
+     */
     public void createRateBA(RateBA rateBA){
         rateRepository.save(rateBA);
     }
 
+    /**
+     * Обновляет процент у сущности RateBA по её id
+     *
+     * @param id Идентификатор сущности RateBA
+     * @param percent Новое значение процента
+     * @return Обновленная сущность RateBA
+     */
     public RateBA updateRateBA(Long id, Double percent){
         RateBA rateBA = rateRepository.findById(id).orElseThrow();
         rateBA.setPercentService(percent);
         rateRepository.save(rateBA);
         return rateRepository.getRateBAById(id);
     }
+    /**
+     * Удаляет сущность RateBA по её id
+     *
+     * @param id Идентификатор сущности RateBA для удаления
+     */
     public void deleteRateBA(Long id){
         rateRepository.deleteById(id);
     }
